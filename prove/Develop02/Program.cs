53150f122d2dff602d8cd30265602e4eaabd50eb
colbymacarthur <colby.mac89@gmail.com>
2024-03-07 19:29:28 -0600

    prepareW02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+        List<string> prompts = new List<string>() {"What positive quality or value can you focus on today?", 
+        "Did you achieve your goals today?", 
+        "Check in with your body-how is it feeling today?", 
+        "What are the positives of your day?", 
+        "An inspirational thought or theme for tomorrow.",
+        "What can you do today to take care of yourself and your needs, even just for a little while?" };
     }
 }
